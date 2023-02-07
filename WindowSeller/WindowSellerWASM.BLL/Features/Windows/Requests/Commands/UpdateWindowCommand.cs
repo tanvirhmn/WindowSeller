9c4e589712adfae40275cfe44403c1418fb8cd6d
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2023-02-07 22:39:43 +0600

    BLl 3

---
+++ WindowSeller/WindowSellerWASM.BLL/Features/Windows/Requests/Commands/UpdateWindowCommand.cs
@@ -0,0 +1,18 @@
+﻿
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowSellerWASM.BLL.DTOs.Window;
+using WindowSellerWASM.BLL.Responses;
+
+namespace WindowSellerWASM.BLL.Features.Window.Requests.Commands
+{
+    public class UpdateWindowCommand : IRequest<BaseCommandResponse>
+    {
+        public WindowDto WindowDto { get; set; }
+
+    }
+}
---
+++ WindowSeller/WindowSellerWASM.BLL/Features/Windows/Requests/Queries/GetWindowListByOrderIdRequest.cs
@@ -0,0 +1,17 @@
+﻿
+
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowSellerWASM.BLL.DTOs.Window;
+
+namespace WindowSellerWASM.BLL.Features.Window.Requests.Queries
+{
+    public class GetWindowListByOrderIdRequest : IRequest<List<WindowDto>>
+    {
+        public long orderId { get; set; }
+    }
+}
