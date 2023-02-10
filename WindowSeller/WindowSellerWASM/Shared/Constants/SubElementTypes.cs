9a01502128220fa048d72efe357e4d377099cf0c
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2023-02-10 19:33:35 +0600

    Day End Push

---
+++ WindowSeller/WindowSellerWASM/Shared/Constants/SubElementTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowSellerWASM.Shared.Constants
+{
+    public class SubelementTypes
+    {
+        public static readonly List<string> Types = new List<string>
+        {
+         "Doors","Window"
+        };
+    }
+}
---
+++ WindowSeller/WindowSellerWASM/Shared/Constants/UnitedStates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowSellerWASM.Shared.Constants
+{
+    public class UnitedStates
+    {
+        public static readonly List<string> StateCodes =
+         "AL, AK, AZ, AR, CA, CO, CT, DE, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY".Split(',').ToList();
+    }
+}
