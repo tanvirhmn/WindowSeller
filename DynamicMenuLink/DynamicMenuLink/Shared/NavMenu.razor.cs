6463a5a658ca0d00acf818a82ac8bb86d5cb06b1
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2023-06-14 17:17:45 +0600

    fixes

---
+++ DynamicMenuLink/DynamicMenuLink/Shared/NavMenu.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DynamicMenuLink.Shared
+{
+    public partial class NavMenu
+    {
+        [Inject] public NavigationManager NavMan { get; set; }
+
+        string counterURL
+        {
+            get
+            {
+                    string url = NavMan.BaseUri;
+
+                    Uri myUri = new Uri(url);
+
+                    string host = myUri.Host;
+                    string port = myUri.Port.ToString();
+
+                return "https://" + host + ":"+ port + "/counter";
+            }
+        }
+
+        string fetchdataURL
+        {
+            get
+            {
+
+                    return NavMan.BaseUri + @"fetchdata";
+
+            }
+        }
+
+        string helloURL
+        {
+            get
+            {
+                    return NavMan.ToAbsoluteUri("hello").ToString();
+            }
+        }
+    }
+}
