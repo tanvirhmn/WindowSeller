d2203d1afd6df49bdcd5fc2eb0b45d13dcf0eb34
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2023-10-09 23:28:00 +0600

    fixes

---
+++ ConsoleProducer/ConsoleProducer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace ConsoleProducer
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ConnectionFactory factory = new ConnectionFactory { HostName = "http://factory-mq.intus.lt:8080/" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "inventory-scan", durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+
+        }
+       
+    }
+}
---
+++ ConsoleProducer/ConsoleRabbitConsumerSignalRHub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsoleRabbitConsumerSignalRHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory { HostName = "http://factory-mq.intus.lt:8080/" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "inventory-scan",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+            };
+            channel.BasicConsume(queue: "inventory-scan",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
