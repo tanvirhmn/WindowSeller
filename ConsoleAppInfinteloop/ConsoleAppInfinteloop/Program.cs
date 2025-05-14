9432f8053a09daa50ea02bae563d34763187d04a
Tanvir Rahman <tanvir@intuswindows.com>
2025-05-14 12:44:19 +0600

    infinteloop

---
+++ ConsoleAppInfinteloop/ConsoleAppInfinteloop/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
+int value = 1000;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Press the Escape (Esc) key to stop the loop.");
+
+while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Escape)
+{
+    Console.WriteLine($"Value: {value}");
+    value++;
+    Thread.Sleep(1000); // Wait for 1 second
+}
+
